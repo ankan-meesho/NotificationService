20b2667c99c58d15f6127bdb01b9012b8ad36f7a
ankan-meesho <ankan.sen@meesho.com>
2025-01-24 22:36:33 -0800

    Delete src directory

--- src/main/java/com/example/demo/utils/AppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.utils;
-
-public class AppConstant {
-    public static final String TOPIC_NAME = "my_topic";
-    public static final String GROUP_ID = "group_id";
-    public static final String INDEX_NAME="phone_index";
-    public static final String MOCK_API_KEY = "Hello1234";
-}
